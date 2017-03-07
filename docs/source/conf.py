1c14ebc76350286adf2fd7ba5d738253d2558188
Andrew Dunai <andunai@gmail.com>
2017-03-07 14:12:12 +0200

    Documentation mocks.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,8 @@
 #
 import os
 import sys
+from unittest.mock import MagicMock
+
 sys.path.insert(0, os.path.abspath('../..'))
 
 
@@ -162,5 +164,10 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
 
-
+MOCK_MODULES = ['pika', 'gevent']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
