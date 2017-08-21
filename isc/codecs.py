8638be879838366c75ae9503f1cc6f0623afd04d
Andrew Dunai <andunai@gmail.com>
2017-08-21 15:26:33 +0300

    Added TypedJSONCodec

--- isc/codecs.py
+++ isc/codecs.py
@@ -1,5 +1,12 @@
 import pickle
 import json
+from uuid import UUID
+from dateutil import parser
+from datetime import datetime
+
+
+class CodecException(Exception):
+    pass
 
 
 class AbstractCodec(object):
@@ -54,3 +61,40 @@ class JSONCodec(AbstractCodec):
 
     def decode(self, payload):
         return json.loads(payload)
+
+
+class TypedJSONCodec(AbstractCodec):
+    """
+    JSON codec implementation with support of timestamps & UUIDs.
+    """
+    content_type = 'typedjson'
+
+    def encode(self, message):
+        encoder = json.JSONEncoder(default=self._encode_object)
+        return encoder.encode(message)
+
+    def _encode_object(self, v):
+        if isinstance(v, datetime):
+            return dict(__object_type='datetime', __object_value=v.isoformat())
+        elif isinstance(v, UUID):
+            return dict(__object_type='uuid', __object_value=v.hex)
+        else:
+            raise CodecException('Don\'t know how to serialize {}'.format(repr(v)))
+
+    def decode(self, payload):
+        return json.JSONDecoder(object_hook=self._decode_object).decode(payload)
+
+    def _decode_object(self, v):
+        try:
+            obj_type = v['__object_type']
+            obj_value = v['__object_value']
+        except:
+            return v
+
+        if obj_type == 'datetime':
+            return parser.parse(obj_value)
+        elif obj_type == 'uuid':
+            return UUID(obj_value)
+        else:
+            raise CodecException('Unknown type: {}'.format(obj_type))
+
