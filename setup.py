28e98e8593a57b8bc32d8b364a90579d3edaf74b
Andrew Dunai <andunai@gmail.com>
2017-09-26 17:35:01 +0300

    Version 0.40

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='isclib',
-    version='0.39',
+    version='0.40',
     description='ISC: Inter-service communication layer for Python. Compatible with gevent.',
     author="Andrew Dunai",
     author_email='andrew@dun.ai',
